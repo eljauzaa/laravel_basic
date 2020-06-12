6fa59bd23467f83a6a3dd19461c1421324466f2a
deddy <deddy.al.jauzaa@gmail.com>
2020-06-12 15:26:06 +0700

    small_edits

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -4,7 +4,7 @@
 <div class="flex-center position-ref full-height">
     <div class="content">
         <div class="title m-b-md">
-            Daftar Pesanan - {{ $id }}
+            Daftar Pesanan anda - {{ $id }}
         </div>
     </div>
 </div>
